fedf1de2dfd845ca7bef791984e6621da64b01f6
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-12-23 00:24:04 +0500

    jlj

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $(document).ready(function () {
     loop: true,
   });
 });
+
+// LOADING
+
+const loading = document.getElementById("loading");
+
+const loadingDuration = 2600; // 2s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
